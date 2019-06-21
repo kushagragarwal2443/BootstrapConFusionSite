73b74e88f1fac5da073eca6ea22639f3de689c35
Kushagra Agarwal <kushagragarwal2443@gmail.com>
2019-06-22 02:15:04 +0530

    JS Housekeeping

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+
+    $("#reservebutton").click(function(){
+
+        $("#ReserveModal").modal("show");
+    });
+
+    $("#loginbutton").click(function(){
+
+        $("#loginModal").modal("show");
+    });
+
+    $(".closesReserveModal").click(function(){
+        $("#ReserveModal").modal("hide");
+    });
+
+    $(".closesLoginModal").click(function(){
+        $("#loginModal").modal("hide");
+    });
+
+
+    $("#mycarousel").carousel({interval:200});
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+            $("#mycarousel").carousel('pause');
+            $("#carouselButton").children("span").removeClass('fa-pause');
+            $("#carouselButton").children("span").addClass('fa-play');
+        }
+        else if ($("#carouselButton").children("span").hasClass('fa-play')){
+            $("#mycarousel").carousel('cycle');
+            $("#carouselButton").children("span").removeClass('fa-play');
+            $("#carouselButton").children("span").addClass('fa-pause');                    
+        }
+    });
+});
